b4641b6fa8514583d60f6c89896ef781551408a8
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2026-03-24 01:04:07 +0530

    add: extract bootstrap

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,27 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-
-import { Logger } from 'nestjs-pino';
-
-import { AppConfigService } from '@app/shared/app-config/app-config.service';
-import { AppLoggerService } from '@app/shared/app-logger/app-logger.service';
+import { appBootstrap } from '@app/shared/app.bootstrap';
 
 import { AuthModule } from './auth.module';
 
-async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AuthModule, {
-    bufferLogs: true,
-  });
-
-  const pinoLogger = app.get(Logger);
-  app.useLogger(pinoLogger);
-  const appConfigService = app.get(AppConfigService);
-  const appLoggerService = await app.resolve(AppLoggerService);
-
-  const PORT = appConfigService.port;
-
-  await app.listen(PORT);
-  appLoggerService.info(`Auth service is running on port ${PORT}`, {
-    context: 'Bootstrap',
-  });
-}
-void bootstrap();
+void appBootstrap(AuthModule);
---
+++ libs/shared/src/app.bootstrap.ts
@@ -0,0 +1,35 @@
+import { Type } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { Logger } from 'nestjs-pino';
+
+import { AppConfigService } from '@app/shared/app-config/app-config.service';
+import { AppLoggerService } from '@app/shared/app-logger/app-logger.service';
+
+export const appBootstrap = async (module: Type<unknown>): Promise<void> => {
+  const isGeneratingSchema = process.env.GENERATE_SCHEMA === 'true';
+  const serviceName = process.env.SERVICE;
+
+  if (isGeneratingSchema) {
+    const app = await NestFactory.create(module, { logger: false });
+    await app.init();
+    // eslint-disable-next-line no-console
+    console.log(`Successfully generated schema for ${serviceName}`);
+    await app.close();
+    process.exit(0);
+  }
+
+  const app = await NestFactory.create(module, { logger: false });
+
+  const pinoLogger = app.get(Logger);
+  app.useLogger(pinoLogger);
+  const appConfigService = app.get(AppConfigService);
+  const appLoggerService = await app.resolve(AppLoggerService);
+
+  const port = appConfigService.port;
+
+  await app.listen(port);
+  appLoggerService.info(`${serviceName} service is running on port ${port}`, {
+    context: 'Bootstrap',
+  });
+};
